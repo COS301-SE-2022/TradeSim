45aea4830ccb8d4d575766790abe996125712f62
siphoxnkosi <u19107383@tuks.co.za>
2022-05-10 11:52:34 +0200

    Added Bootstrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app =  Flask(__name__, template_folder='apps/client/templates')
+Bootstrap(app)
 
 @app.route("/")
 def index():
