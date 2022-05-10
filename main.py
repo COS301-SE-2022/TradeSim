bc0782698e4242a5e3ad9efa8a39b832f4d8bbb7
siphoxnkosi <u19107383@tuks.co.za>
2022-05-10 15:51:36 +0200

    Added Login Route

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ Bootstrap(app)
 def index():
     return render_template("login.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
