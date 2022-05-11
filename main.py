b50576737d0dc6005ecf0e672b5e930439308127
siphoxnkosi <u19107383@tuks.co.za>
2022-05-11 15:15:44 +0200

    Added home route

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def login():
 def register():
     return render_template("signup.html")
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
