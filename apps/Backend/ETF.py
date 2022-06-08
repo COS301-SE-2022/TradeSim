462237ee6ebde5dcd000be3f005595adb5036b9d
Regan Shen <u20460067@tuks.co.za>
2022-06-08 23:38:03 +0200

    (BackEnd) : 2 functions added
    The functions that were added is the ability to remove a specific company as well as the ability to add a specific company

--- apps/Backend/ETF.py
+++ apps/Backend/ETF.py
@@ -1,60 +1,61 @@
 import apiCalls
 class ETF:
+
+    stocksConfirmedIn = []
     def __init__(self,UserID, etfID, rules):
         self.UserID = UserID
         self.etfIF = etfID
         self.rules = rules
-
+        self.listOfAllStocks = []
 
 
     def createETF(self):
-        print(self.rules)
-        self.prioritizeRules()
-        print(self.rules)
 
-        # This is function that contains all the new rules when the person submits
-        # data is the array of the JSon of all the data recieved
-        names = apiCalls.listallcompanies()
+
+        self.prioritizeRules()
+
+        self.listOfAllStocks = apiCalls.listallcompanies()
+
+        print(self.listOfAllStocks)
+        for x in self.rules:
+            code = x[0]
+            parameters = x[1]
+            self.convertCodeToEtf(code,parameters)
+
+        print(self.listOfAllStocks)
+
+
         # first = apiCalls.getShareMarketEarn(names, "2018-06-29")
         # first contains all the shares with corresponding share Price, Market Cap and earnings, first is a dictionary so the index is the ticker of the share
-
         # apiCalls.CompaniesByIndustry(106)
-        country = apiCalls.getCountry(
-            ['CRDF', 'CRDO', 'CREE', 'CRI', 'CRIS', 'CRL', 'CRM', 'CRNC', 'CROX', 'CRR', 'CRS', 'CRSP', 'CRSR', 'CRTO',
-             'CRUS', 'CRVS', 'CRWD', 'CRWS', 'CRY', 'CRZO'])
-        print(country)
+        # country = apiCalls.getCountry(
+        #     ['CRDF', 'CRDO', 'CREE', 'CRI', 'CRIS', 'CRL', 'CRM', 'CRNC', 'CROX', 'CRR', 'CRS', 'CRSP', 'CRSR', 'CRTO',
+        #      'CRUS', 'CRVS', 'CRWD', 'CRWS', 'CRY', 'CRZO'])
+        # print(country)
 
-        # for rule in self.rules:
-        #     code = rule[0]
-        #     parameters = rule[1]
-        #     print(code)
-        #     print(parameters)
 
-    def prioritizeRules(self):
-        newRuleList = []
-        countPrioritize = 0
-        while len(newRuleList) != len(self.rules):
-            for rule in self.rules:
-                code = rule[0]
-                parameters = rule[1]
-                if countPrioritize == 0:
-                    if code == "000" or code == "101":
-                        newRuleList.append(rule)
-                if countPrioritize == 1:
-                    if code == "011" or code == "012" or code == "013" or code == "104":
-                        newRuleList.append(rule)
-                if countPrioritize == 3:
-                    if code == "001" or code == "002" or code == "003" or code == "102" or code == "103" or code == "105":
-                        newRuleList.append(rule)
-            countPrioritize = countPrioritize +1
+    def code000(self,ticker):
+        # First we will now remove the stock in this particular moment
+        newListOFstocks = []
+        for x in self.listOfAllStocks:
+            if x!=ticker:
+                newListOFstocks.append(x)
 
-        self.rules = newRuleList
+        self.listOfAllStocks = newListOFstocks
+
+    def code101(self, ticker):
+            #First we want to check if the stock exists
+            if ticker in self.listOfAllStocks:
+                self.stocksConfirmedIn.append(ticker)
+            else:
+                print("Stock Doesn't Exist")
+                # So we will need to return some way of saying that this stock doesnt exist
 
     def convertCodeToEtf(self,code, parameters):
-        print(self.UserID)
         # In here we will have many switch statements when we get the code and its parameters in order to know what to do with it
         if code == "000":
             ticker = parameters[0]
+            self.code000(ticker)
             # Reject by specific companies
             # The ticker is our only parameter
         elif code == "001":
@@ -91,6 +92,7 @@ class ETF:
         elif code == "101":
             ticker = parameters[0]
             Percentage = parameters[1]
+            self.code101(ticker)
             # companies by name or ticker.
         elif code == "102":
             SectorID = parameters[0]
@@ -124,3 +126,23 @@ class ETF:
         elif code == "202":
             reconsiderPeriodInWeeks = parameters[0]
             # time period in which it must reconsider its stocks
+
+    def prioritizeRules(self):
+        newRuleList = []
+        countPrioritize = 0
+        while len(newRuleList) != len(self.rules):
+            for rule in self.rules:
+                code = rule[0]
+                parameters = rule[1]
+                if countPrioritize == 0:
+                    if code == "000" or code == "101":
+                        newRuleList.append(rule)
+                if countPrioritize == 1:
+                    if code == "011" or code == "012" or code == "013" or code == "104":
+                        newRuleList.append(rule)
+                if countPrioritize == 3:
+                    if code == "001" or code == "002" or code == "003" or code == "102" or code == "103" or code == "105":
+                        newRuleList.append(rule)
+            countPrioritize = countPrioritize +1
+
+        self.rules = newRuleList
