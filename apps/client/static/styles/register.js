c60baa26811de469e00450211ac199eef5258439
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2022-07-20 17:01:30 +0200

    Update register.js

--- apps/client/static/styles/register.js
+++ apps/client/static/styles/register.js
@@ -1,6 +1,8 @@
 function validateform() {
+
     var flag = true;
     var name = document.myform.name.value;
+    var email = document.myform.email.value;
     var password = document.myform.password.value;
 
     if (name == null || name == "") {
@@ -12,7 +14,6 @@ function validateform() {
         flag = false;
     }
 
-
     var firstpassword = document.myform.password.value;
     var secondpassword = document.myform.cpsw.value;
 
@@ -21,19 +22,49 @@ function validateform() {
         flag = false;
     }
 
-    if (flag == true) {
-        return true;
-    } else {
-        return false;
-    }
+
+        alert("test1")
+        password = hash(password);
+        const details =
+        {
+            "Data" : [name , email, password]
+        }
+
+         fetch("http://127.0.0.1:6969/register",
+        {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+        // Strigify the payload into JSON:
+        body:JSON.stringify(details)}
+         ).then(response=> response.json())
+             .then(data =>{
+             console.log(data)
+             const jd = JSON.parse(data)
+             document.getElementById("response").innerHTML = jd.error;
+         });
+
+
+
+
 
 }
 
-function checkETF() {
-    var etfname = document.myform.etfName.value;
-    if (etfname == null || etfname == "") {
-        alert("Please name your ETF");
-        flag = false;
-    }
-    return true;
+
+ function hash(p)
+ {
+  var hash = 0, i, c;
+  if (p.length === 0)
+  {
+    return hash;
+  }
+  for (i = 0; i < p.length; i++)
+  {
+    c   = p.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + c;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
 }
