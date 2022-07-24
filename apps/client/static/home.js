17ddf58f090412a45cb72880bc9770f94c83ca13
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2022-07-24 14:04:22 +0200

    Update home.js

--- apps/client/static/home.js
+++ apps/client/static/home.js
@@ -125,14 +125,7 @@ function getGraph(name, uID, etfid, rules, amount, date, chartnum)
           y: yA,
           mode:"lines"
          }];
-
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0');
-        var yyyy = today.getFullYear();
-
-        today = mm + '/' + dd + '/' + yyyy;
-
+         
         var layout =
         {
           xaxis: {title: "date"},
