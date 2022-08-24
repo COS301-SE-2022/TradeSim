078ccbbfb283ff131986e5400f1117881f286a65
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2022-08-24 11:16:16 +0200

    Revert "New Compare Page & Manage Template"

--- apps/client/static/ticker.js
+++
@@ -1,4 +0,0 @@
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, options);
-  });
