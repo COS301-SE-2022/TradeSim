bae77f7edcab38c4ddf1fcab1964e43e91c8f600
siphoxnkosi <u19107383@tuks.co.za>
2022-08-28 16:15:19 +0200

    Displayed graph after finding ticker

--- apps/client/static/ticker.js
+++ apps/client/static/ticker.js
@@ -1,3 +1,6 @@
+var graphName;
+var obj;
+
 function searchTicker() {
     var showTable = document.getElementById("results");
     if (showTable.style.display === "none") {
@@ -8,9 +11,9 @@ function searchTicker() {
     console.log(tick);
     //getCompanyInformation(tick)
     const details =
-    {
-        "ticker" : tick
-    }
+        {
+            "ticker": tick
+        }
     fetch("http://127.0.0.1:6969/tickerInfo",
         {
             method: 'POST',
@@ -18,14 +21,51 @@ function searchTicker() {
                 'Content-type': 'application/json',
                 'Accept': 'application/json'
             },
-        // Strigify the payload into JSON:
-        body:JSON.stringify(details)}
-         ).then(response=> response.json())
-             .then(data =>{
-             console.log("data",data)
-                 if (data.Ticker != null)
-                    document.getElementById("response").innerHTML += `<tr><td>${data['Company Name']}</td><td>${data.Ticker}</td><td>${data.Sector}</td><td>${data.Summary}</td></tr>`;
-                 else
-                     document.getElementById("response").innerHTML += `<tr><td>Company with ticker name: <b>${tick}</b> not found. Did you mean: <b>${data.PossibleStock}</b>?</td></tr>`;
-             });
+            // Strigify the payload into JSON:
+            body: JSON.stringify(details)
+        }
+    ).then(response => response.json())
+        .then(data => {
+            console.log("data", data)
+            graphName = data['Company Name']
+            console.log("name of Graph", graphName);
+            obj = data.PriceHistory;
+            console.log("Price History", obj);
+            if (data.Ticker != null)
+                document.getElementById("response").innerHTML = `<tr><td>${data['Company Name']}</td><td>${data.Ticker}</td><td>${data.Sector}</td><td>${data.Summary}</td></tr>`;
+            else
+                document.getElementById("response").innerHTML = `<tr><td>Company with ticker name: <b>${tick}</b> not found. Did you mean: <b>${data.PossibleStock}</b>?</td></tr>`;
+
+            getGraph(graphName, obj);
+        });
+}
+
+function getGraph(name, obj) {
+
+    var xA = [];
+    var yA = [];
+    for (let key in obj) {
+        xA.push(key)
+        yA.push(obj[key])
+        // console.log(key + " -> " + obj[key]);
+    }
+    // console.log("xA", xA)
+    // console.log("yA", yA)
+
+
+    var data2 = [{
+        x: xA,
+        y: yA,
+        mode: "lines"
+    }];
+
+    var layout =
+        {
+            xaxis: {title: "date"},
+            yaxis: {title: "price in dollars"},
+            title: name
+        };
+    console.log("Drawing Graph")
+    Plotly.newPlot("notes", data2, layout);
+
 }
