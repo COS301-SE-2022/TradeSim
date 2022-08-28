e1eb748af4642508bf106ad85dbe66a6cf0771e0
siphoxnkosi <u19107383@tuks.co.za>
2022-08-28 14:39:18 +0200

    Connected Ticker Page to Backend

--- apps/client/static/ticker.js
+++ apps/client/static/ticker.js
@@ -1,12 +1,28 @@
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, {
-        data:{
-          'Apple':null,
-          'Microsoft':null,
-          'Amazon':null,
-          'Nike':null,
-          'Bing':null,
-        },
-    });
-  });
+function searchTicker() {
+    var showTable = document.getElementById("results");
+    if (showTable.style.display === "none") {
+        showTable.style.display = "block";
+    }
+    var tick = document.getElementById('tickerInput').value;
+    tick = tick.toUpperCase();
+    console.log(tick);
+    //getCompanyInformation(tick)
+    const details =
+    {
+        "ticker" : tick
+    }
+    fetch("http://127.0.0.1:6969/tickerInfo",
+        {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+        // Strigify the payload into JSON:
+        body:JSON.stringify(details)}
+         ).then(response=> response.json())
+             .then(data =>{
+             console.log("data",data)
+             document.getElementById("response").innerHTML += `<tr><td>${data['Company Name']}</td><td>${data.Ticker}</td><td>${data.Sector}</td><td>${data.Summary}</td></tr>`;
+         });
+}
