0417a65ac788fc73ab4479eebd27b187ebc7b57d
siphoxnkosi <u19107383@tuks.co.za>
2022-08-28 12:00:13 +0200

    Created Ticker Template

--- apps/client/static/ticker.js
+++ apps/client/static/ticker.js
@@ -1,4 +1,12 @@
  document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, options);
+    var instances = M.Autocomplete.init(elems, {
+        data:{
+          'Apple':null,
+          'Microsoft':null,
+          'Amazon':null,
+          'Nike':null,
+          'Bing':null,
+        },
+    });
   });
