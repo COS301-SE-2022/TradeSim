22f1cb57337a0d93e6b825fb8f36628a9656d678
siphoxnkosi <u19107383@tuks.co.za>
2022-09-12 17:14:27 +0200

    Updated Ticker Page Layout

--- apps/client/static/ticker.js
+++ apps/client/static/ticker.js
@@ -4,10 +4,10 @@ var obj;
 function searchTicker() {
     const loaderDiv = document.getElementById('loader');
     loaderDiv.classList.add('show');
-    var showTable = document.getElementById("results");
-    if (showTable.style.display === "none") {
-        showTable.style.display = "block";
-    }
+    // var showTable = document.getElementById("results");
+    // if (showTable.style.display == "none") {
+    //     showTable.style.display = "block";
+    // }
     var tick = document.getElementById('tickerInput').value;
     tick = tick.toUpperCase();
     console.log(tick);
@@ -36,10 +36,12 @@ function searchTicker() {
             const loaderDiv = document.getElementById('loader');
             loaderDiv.classList.remove('show');
             if (data.Ticker != null) {
-                document.getElementById("response").innerHTML = `<tr><td>${data['Company Name']}</td><td>${data.Ticker}</td><td>${data.Sector}</td><td>${data.Summary}</td></tr>`;
+                document.getElementById("card-title").innerHTML = `<div class="row"><div class="col-8">${data.Ticker}:${data['Company Name']}</div><div class="col-4"><img src=${data.Logo} height="100%" width="100%"></div></div>`;
+                console.log(data['Company Name']);
+                document.getElementById("response").innerHTML = `<table><tbody><tr><td><b>Industry: </b>${data.Industry}</td></tr><tr><td>${data.Summary}</td></tr></tbody></table>`;
                 getGraph(graphName, obj);
             } else {
-                document.getElementById("response").innerHTML = `<tr><td>Company with ticker name: <b>${tick}</b> not found. Did you mean: <b>${data.PossibleStock}</b>?</td></tr>`;
+                document.getElementById("response").innerHTML = `<table><tbody><tr><td>Company with ticker name: <b>${tick}</b> not found. Did you mean: <b>${data.PossibleStock}</b>?</td></tr></tbody></table>`;
             }
         });
 }
