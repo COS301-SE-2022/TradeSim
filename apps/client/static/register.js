716cf1880682edded8a2ab6a8919840a1029a96c
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2022-09-22 17:59:43 +0200

    Update register.js

--- apps/client/static/register.js
+++ apps/client/static/register.js
@@ -30,7 +30,7 @@ function validateform() {
         }
 
     if (flag) {
-        fetch("http://127.0.0.1:6969/register",
+        fetch("http://ec2-18-208-221-145.compute-1.amazonaws.com:6969/register",
             {
                 method: 'POST',
                 headers: {
