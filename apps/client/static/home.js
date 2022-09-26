c6773a51562a91990072f02110812fe90aebf349
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2022-09-26 19:58:03 +0200

    Update home.js

--- apps/client/static/home.js
+++ apps/client/static/home.js
@@ -20,7 +20,7 @@ function getETFS() {
             "Data": [userID]
         }
 
-    fetch("http://ec2-18-208-221-145.compute-1.amazonaws.com:6969/getETFS",
+    fetch("http://ec2-54-82-241-49.compute-1.amazonaws.com:6969/getETFS",
         {
             method: 'POST',
             headers: {
@@ -100,7 +100,7 @@ function confirm() {
             "Data": [userID]
         }
 
-    fetch("http://ec2-18-208-221-145.compute-1.amazonaws.com:6969/getETFS",
+    fetch("http://ec2-54-82-241-49.compute-1.amazonaws.com:6969/getETFS",
         {
             method: 'POST',
             headers: {
@@ -156,7 +156,7 @@ function getETFS2() {
             "Data": [userID]
         }
 
-    fetch("http://ec2-18-208-221-145.compute-1.amazonaws.com:6969/getETFS",
+    fetch("http://ec2-54-82-241-49.compute-1.amazonaws.com:6969/getETFS",
         {
             method: 'POST',
             headers: {
@@ -208,7 +208,7 @@ function getGraph(name, uID, etfid, rules, amount, date, chartnum) {
         }
 
     console.log(JSON.stringify(details2));
-    fetch("http://ec2-18-208-221-145.compute-1.amazonaws.com:6969/createRules",
+    fetch("http://ec2-54-82-241-49.compute-1.amazonaws.com:6969/createRules",
         {
             method: 'POST',
             headers: {
@@ -290,7 +290,7 @@ function getNews(value) {
         console.log("category", value)
     }
 
-    fetch("http://ec2-18-208-221-145.compute-1.amazonaws.com:6969/news",
+    fetch("http://ec2-54-82-241-49.compute-1.amazonaws.com:6969/news",
         {
             method: 'POST',
             headers: {
@@ -348,7 +348,7 @@ function getGraph3(name, uID, etfid, rules, amount, date, chartnum) {
         }
 
     console.log(JSON.stringify(details2));
-    fetch("http://ec2-18-208-221-145.compute-1.amazonaws.com:6969/createRules",
+    fetch("http://ec2-54-82-241-49.compute-1.amazonaws.com:6969/createRules",
         {
             method: 'POST',
             headers: {
