e759f06ef21a6664a952cbeb0b90593a3e02d487
Regan Shen <u20460067@tuks.co.za>
2022-09-28 22:41:52 +0200

    (Testing) : Integration
    Added the final integration tests to the system

--- apps/Backend/IntegrationTesting.py
+++ apps/Backend/IntegrationTesting.py
@@ -1,6 +1,8 @@
 import unittest
 import apiCalls
 import info
+import ETF
+import AiFactor
 
 
 
@@ -98,11 +100,232 @@ class Tests(unittest.TestCase):
         self.assertIsNotNone(data)
         self.assertTrue(size > 0, "Test if the news function returns a value")
 
-    # def test_StockInformation(self):
-    #     testTicker = "AAPL"
-    #     response = info.stockInformation(testTicker)
-    #     knownAnswer = {"Found" : "True", "Ticker" : "AAPL"}
-    #     self.assertEqual(response,knownAnswer,"Test if stock information is correct")
+    def test_StockInformation(self):
+        testTicker = "AAPL"
+        fileLocation = 'databases\industries.csv'
+        response = info.stockInformation(testTicker,fileLocation)
+        knownAnswer = 'True'
+        self.assertIsNot(response == None, "Test to see if not None for stock")
+        self.assertEqual(response['Found'],knownAnswer,"Test if stock information is correct")
+
+    def test_newsInformation_empty(self):
+        response = info.newsInformation("")
+        knownAnswer = 'True'
+        print(response)
+        self.assertIsNot(response == None, "Test to see if not None for news")
+        self.assert_(len(response)>0,"Test if newsInformation returns news even if the parameter is empty")
+
+    def test_newsInformation_withParameter(self):
+        response = info.newsInformation("Forex")
+        knownAnswer = 'True'
+        print(response)
+        self.assertIsNot(response == None, "Test to see if not None for news")
+        self.assert_(len(response)>0,"Test if newsInformation returns news relevant to category")
+
+    def test_createEtf_noRule(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = []
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for createETF")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns stocks even when no rules are given")
+
+    def test_createEtf_testRule000(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["000",["AAPL"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = "AAPL"
+
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule000")
+        self.assertIsNot(knwon_response in response,"Test if createETF returns the correct output when rejecting a company")
+
+    def test_createEtf_testRule001(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["001", ["100"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule001")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when rejecting a specific sector")
+
+    def test_createEtf_testRule002(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["002", ["100000"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule002")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when rejecting a specific industry")
+
+    def test_createEtf_testRule003(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["003", ["US"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule003")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when rejecting a specific country")
+
+    def test_createEtf_testRule011(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["011", [10,100000000000]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule011")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when setting a min and max market cap")
+
+    def test_createEtf_testRule012(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["012", [10,100000000000]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule012")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when setting a min and max earning")
+
+    def test_createEtf_testRule013(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["013", [10,250]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule013")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when setting a min and max price")
+
+    def test_createEtf_testRule101(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["101", ["AAPL", "60"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 3000
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule101")
+        self.assert_(response["AAPL"]>knwon_response,"Test if createETF returns the correct output when an individual asks for certain stocks")
+
+    def test_createEtf_testRule102(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["102", ["100","50","2"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule102")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when setting a specific sector")
+
+    def test_createEtf_testRule103(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["103", ["100000","50","2"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule103")
+        self.assert_(len(response)>knwon_response,"Test if createETF returns the correct output when setting a specific industry")
+
+    def test_createEtf_testRule104(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["104", ["50","10"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 10
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule104")
+        self.assert_(len(response)>=knwon_response,"Test if createETF returns the correct output when grabbing 10 stocks with the highest market Cap")
+
+    def test_createEtf_testRule105(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["105",["US","50","5"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule105")
+        self.assert_(len(response)>=knwon_response,"Test if createETF returns the correct output when grabbing stocks from a certain exchange")
+
+
+    def test_createEtf_testRule106(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["106", ["50","10"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 10
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule106")
+        self.assert_(len(response)>=knwon_response,"Test if createETF returns the correct output when grabbing 10 stocks with the highest revenue")
+
+    def test_createEtf_testRule200(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["200", ["32"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule200")
+        self.assert_(len(response)>=knwon_response,"Test if createETF returns the correct output when adding the rebalancing rule")
+
+    def test_createEtf_testRule202(self):
+        mock_userID = 0
+        mock_etfID = 0
+        mock_Rules = [["202", ["32"]]]
+        mock_Date = "2022-09-15"
+        mock_amount = 1000000
+        mock_etf = ETF.ETF(mock_userID, mock_etfID, mock_Rules, mock_Date, mock_amount)
+        response = mock_etf.createETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for create ETF with rule202")
+        self.assert_(len(response)>=knwon_response,"Test if createETF returns the correct output when adding the reconsidering rule")
+
+    def test_AiFactor(self):
+        mock_Date = "2022-09-15"
+        mock_seedValue = 31
+        mock_file = 'databases\industries.csv'
+        AiTing = AiFactor.AiFactor(mock_Date,mock_seedValue,mock_file)
+        response = AiTing.generateRandomETF()
+        knwon_response = 0
+        self.assertIsNot(response == None, "Test to see if not None for generating AiETF")
+        self.assert_(len(response) >= knwon_response,"Test if AiFactor will generate a ETF without any errors")
+
 
 if __name__ == '__main__':
     unittest.main()
